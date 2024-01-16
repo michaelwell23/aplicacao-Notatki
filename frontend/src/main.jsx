56abb138c6cfd4ab8463a6978139ec0e3fe395e7
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-01-16 15:41:45 -0300

    Adicionando projeto ao repositório

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
+import App from './App.jsx';
+
+import GlobalStyles from './styles/global.js';
+import theme from './styles/theme';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/Details/index.jsx
@@ -0,0 +1,15 @@
+import { Container } from './styles';
+
+import { Button } from '../../components/button';
+
+function Details() {
+  return (
+    <Container>
+      <Button title='Entrar' />
+      <Button title='Login' />
+      <Button title='Sair' />
+    </Container>
+  );
+}
+
+export default Details;
