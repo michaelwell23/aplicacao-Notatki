10e68002ba018be3f8b9006fcffe73492edd05a3
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-01-18 14:09:32 -0300

    Concluindo a pagina Details

--- frontend/src/pages/Details/index.jsx
+++ frontend/src/pages/Details/index.jsx
@@ -1,9 +1,10 @@
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 
-import { Header } from '../../components/Header';
-import { Section } from '../../components/Section';
-import { Button } from '../../components/Button';
 import { Tag } from '../../components/Tag';
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 function Details() {
   return (
@@ -11,24 +12,46 @@ function Details() {
       <Container>
         <Header />
 
-        <Section title='Links úteis'>
-          <Links>
-            <li>
-              <a href='/'>www.blogdodev.com.br</a>
-            </li>
-            <li>
-              <a href='/'>www.aprendizadoprogramacao.com.br</a>
-            </li>
-          </Links>
-        </Section>
+        <main>
+          <Content>
+            <ButtonText title='Excluir Nota' />
 
-        <Section title='Marcadores'>
-          <Tag title='Express' />
-          <Tag title='NodeJS' />
-          <Tag title='Api-REST' />
-        </Section>
+            <h1>Introdução ao Node.js</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              ultricies nisl eget purus malesuada pharetra. Nunc nec nunc vitae
+              nulla malesuada dictum sit amet ut purus. Vivamus porttitor massa
+              tellus, id varius ligula rhoncus quis. Praesent luctus mattis diam
+              id euismod. Aenean commodo ultricies metus, sed dapibus massa
+              placerat vel. Integer facilisis dapibus nisi, nec lobortis diam
+              convallis in. Pellentesque vel lacus nisi. Phasellus sagittis quam
+              purus, id lobortis neque elementum ac. Morbi rutrum neque ante, a
+              faucibus mauris interdum eget. Cras at dictum ex. Curabitur felis
+              sapien, finibus at metus at, rhoncus efficitur ligula. Duis
+              pulvinar ligula eget magna rhoncus rutrum. Donec iaculis quam quis
+              magna rhoncus varius. Morbi bibendum lorem tempor sagittis auctor.
+            </p>
 
-        <Button title='Voltar' />
+            <Section title='Links úteis'>
+              <Links>
+                <li>
+                  <a href='/'>www.blogdodev.com.br</a>
+                </li>
+                <li>
+                  <a href='/'>www.aprendizadoprogramacao.com.br</a>
+                </li>
+              </Links>
+            </Section>
+
+            <Section title='Marcadores'>
+              <Tag title='Express' />
+              <Tag title='NodeJS' />
+              <Tag title='Api-REST' />
+            </Section>
+
+            <Button title='Voltar' />
+          </Content>
+        </main>
       </Container>
     </>
   );
