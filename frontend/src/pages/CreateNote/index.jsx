7950477013708b9cfba3785be09a2e544beae167
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-02-08 14:17:23 -0300

    cadastrando notas e validação de campos

--- frontend/src/pages/CreateNote/index.jsx
+++ frontend/src/pages/CreateNote/index.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { api } from '../../services/api';
 
 import { Input } from '../../components/Input';
 import { Header } from '../../components/Header';
@@ -8,14 +11,18 @@ import { NoteItem } from '../../components/NoteItem';
 import { Textarea } from '../../components/Textarea';
 
 import { Container, Form } from './styles';
-import { Link } from 'react-router-dom';
 
 export function CreateNote() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState('');
 
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState('');
+
+  const navegate = useNavigate();
 
   function handleAddLink() {
     setLinks((prevState) => [...prevState, newLink]);
@@ -35,6 +42,33 @@ export function CreateNote() {
     setTags((prevState) => prevState.filter((tag) => tag !== tagDeleted));
   }
 
+  async function handleNewNote() {
+    if (!title) {
+      return alert('Digite um titulo para adicionar uma nota.');
+    }
+
+    if (newLink) {
+      return alert(
+        'Há um Link que não foi adicionada, clique no + para adiciona-lo antes de proceguir ou então deixe o campo vazio.'
+      );
+    }
+
+    if (newTag) {
+      return alert(
+        'Há uma tag que não foi adicionada, clique no + para adiciona-la antes de proceguir ou então deixe o campo vazio.'
+      );
+    }
+
+    await api.post('/notes', {
+      title,
+      description,
+      tags,
+      links,
+    });
+    alert('Nota criada com sucesso!');
+    navegate('/');
+  }
+
   return (
     <Container>
       <Header />
@@ -45,8 +79,14 @@ export function CreateNote() {
             <h1>Criar nota</h1>
             <Link to='/'>Voltar</Link>
           </header>
-          <Input placeholder='Titulo' />
-          <Textarea placeholder='Observações' />
+          <Input
+            placeholder='Titulo'
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Textarea
+            placeholder='Observações'
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
           <Section title='Links uteis'>
             {links.map((link, index) => (
@@ -84,7 +124,7 @@ export function CreateNote() {
               />
             </div>
           </Section>
-          <Button title='Salvar' />
+          <Button title='Salvar' onClick={handleNewNote} />
         </Form>
       </main>
     </Container>
